8e6c796fb815979cdad24aa488d24f99f37bf5a4
Ryan <taiq@student.unimelb.edu.au>
2020-05-10 19:34:02 +1000

    update test

--- backend/backend/test.py
+++ backend/backend/test.py
@@ -26,7 +26,6 @@ def testFetchText():
     url = 'http://localhost:5000/cluster/text'
     payload= {'database': 'lockdown_ade'}
     r = requests.post(url, data=json.dumps(payload), headers=headers)
-
     data = r.json()
     print(len(data['lockdown_ade']))
     print(data)
